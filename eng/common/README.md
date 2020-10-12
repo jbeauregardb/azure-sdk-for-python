2fdbd41657ebb153c9ff7e4f368da1b2d21c56a0
Chidozie Ononiwu <chononiw@microsoft.com>
2020-10-12 11:12:28 -0700

    Fix github verb names

--- eng/common/README.md
+++ eng/common/README.md
@@ -17,8 +17,9 @@ The 'Sync eng/common directory' PRs will be created in the language repositories
 
 1. Create a PR (**Tools PR**) in the `azure - sdk - tools` Repo with changes to eng/common directory.
 2. `azure-sdk-tools - sync - eng-common` pipeline is triggered for the **Tools PR**
-3. The  `azure-sdk-tools - sync - eng-common` pipeline queues test runs using the dotnet, js, java and python template pipelines. This help to test the recent changes in the **Tools PR**
+3. The  `azure-sdk-tools - sync - eng-common` pipeline queues test runs using the dotnet, js, java and python template pipelines. This helps to test the recent changes in the **Tools PR**
 4. If there are changes in the **Tools PR** that will affect the release stage you should approve the release test pipelines by clicking the approval gate. The test (template) pipeline will automatically release the next eligible version without needing manual intervention for the versioning. Please approve your test releases as quickly as possible. A race condition may occur due to someone else queueing the pipeline and going all the way to release using your version while yours is still waiting. If this occurs manually rerun the pipeline that failed.
 5. Repeat step 1 - 4 by pushing new changes to your **Tools PR** Do this until you have satisfactory test runs all the way to release of the template package if necessary.
-6. Sign off on next stage of the sync pipeline using the approval gate. The CreateSyncPRs stage will create the sync PR in the various language repos. Before doing this you need to be satisfied with the testing from the previous steps. This stage will append the `auto-merge` label to the **Sync PRs** as well as the **Tools PR**.
+6. Sign off on next stage of the sync pipeline using the approval gate. The CreateSyncPRs stage will create the sync PR in the various language repos. Before doing this you need to be satisfied with the testing from the previous steps. This stage will append the `auto-merge` label to the **Sync PRs**.
 6. Go review and approve each of your **Sync PRs**. The merging will happen automatically.
+7. Sign Off on the VerifyAndMerge stage. This will merge any remaining open **Sync PR** and also append `auto-merge` to the **Tools PR**.
--- eng/common/scripts/Invoke-GitHubAPI.ps1
+++ eng/common/scripts/Invoke-GitHubAPI.ps1
@@ -1,13 +1,13 @@
 $GithubAPIBaseURI = "https://api.github.com/repos"
 
-function Get-Headers ($token) {
+function Get-GitHubHeaders ($token) {
   $headers = @{
     Authorization = "bearer $token"
   }
   return $headers
 }
 
-function InvokePost {
+function Invoke-GitHubAPIPost {
   param (
     [Parameter(Mandatory = $true)]
     $apiURI,
@@ -21,13 +21,13 @@ function InvokePost {
     -Method POST `
     -Body ($body | ConvertTo-Json) `
     -Uri $apiURI `
-    -Headers (Get-Headers -token $token) `
+    -Headers (Get-GitHubHeaders -token $token) `
     -MaximumRetryCount 3
 
   return $resp
 }
 
-function InvokePatch {
+function Invoke-GitHubAPIPatch {
   param (
     [Parameter(Mandatory = $true)]
     $apiURI,
@@ -41,13 +41,13 @@ function InvokePatch {
     -Method PATCH `
     -Body ($body | ConvertTo-Json) `
     -Uri $apiURI `
-    -Headers (Get-Headers -token $token) `
+    -Headers (Get-GitHubHeaders -token $token) `
     -MaximumRetryCount 3
 
   return $resp
 }
 
-function InvokeGet {
+function Invoke-GitHubAPIGet {
   param (
     [Parameter(Mandatory = $true)]
     $apiURI,
@@ -59,7 +59,7 @@ function InvokeGet {
     $resp = Invoke-RestMethod `
       -Method GET `
       -Uri $apiURI `
-      -Headers (Get-Headers -token $token) `
+      -Headers (Get-GitHubHeaders -token $token) `
       -MaximumRetryCount 3
   }
   else {
@@ -78,7 +78,7 @@ function SplitMembers ($membersString)
   return @($membersString.Split(",") | % { $_.Trim() } | ? { return $_ })
 }
 
-function ListPullRequests {
+function List-PullRequests {
   param (
     [Parameter(Mandatory = $true)]
     $RepoOwner,
@@ -102,10 +102,10 @@ function ListPullRequests {
   if ($Sort) { $uri += "sort=$Sort&" }
   if ($Direction){ $uri += "direction=$Direction&" }
 
-  return InvokeGet -apiURI $uri
+  return Invoke-GitHubAPIGet -apiURI $uri
 }
 
-function AddIssueComment {
+function Add-IssueComment {
   param (
     [Parameter(Mandatory = $true)]
     $RepoOwner,
@@ -125,11 +125,11 @@ function AddIssueComment {
     body = $Comment
   }
 
-  return InvokePost -apiURI $uri -body $parameters -token $AuthToken
+  return Invoke-GitHubAPIPost -apiURI $uri -body $parameters -token $AuthToken
 }
 
 # Will add labels to existing labels on the issue
-function AddIssueLabels {
+function Add-IssueLabels {
   param (
     [Parameter(Mandatory = $true)]
     $RepoOwner,
@@ -150,11 +150,11 @@ function AddIssueLabels {
     labels = $labelAdditions
   }
 
-  return InvokePost -apiURI $uri -body $parameters -token $AuthToken
+  return Invoke-GitHubAPIPost -apiURI $uri -body $parameters -token $AuthToken
 }
 
 # Will add assignees to existing assignees on the issue
-function AddIssueAssignees {
+function Add-IssueAssignees {
   param (
     [Parameter(Mandatory = $true)]
     $RepoOwner,
@@ -175,12 +175,12 @@ function AddIssueAssignees {
     assignees = $assigneesAdditions
   }
 
-  return InvokePost -apiURI $uri -body $parameters -token $AuthToken
+  return Invoke-GitHubAPIPost -apiURI $uri -body $parameters -token $AuthToken
 }
 
 # For labels and assignee pass comma delimited string, to replace existing labels or assignees.
 # Or pass white space " " to remove all labels or assignees
-function UpdateIssue {
+function Update-Issue {
   param (
     [Parameter(Mandatory = $true)]
     $RepoOwner,
@@ -216,5 +216,5 @@ function UpdateIssue {
     $parameters["assignees"] = $assigneesAdditions
   }
 
-  return InvokePatch -apiURI $uri -body $parameters -token $AuthToken
+  return Invoke-GitHubAPIPatch -apiURI $uri -body $parameters -token $AuthToken
 }
